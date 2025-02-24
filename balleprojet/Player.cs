3875d27034ec752c1874d13a2cd79f73028e73c7
Jazztel20 <pb58unc@eduvaud.ch>
2025-02-24 14:43:14 +0100

    chore: ajout de commentaires pour presque toutes les lignes de code de la classe

--- balleprojet/Player.cs
+++ balleprojet/Player.cs
@@ -1,16 +1,45 @@
-﻿namespace balleprojet
+﻿/********************************************************************************
+ Programmation Orientée Objet en C#
+ Projet: Jeu de la balle
+ Prénom et nom : Matias Denis
+ Cours: I320
+ Classe: FID1
+ *******************************************************************************/
+
+// Déclaration de l'espace de noms "balleprojet"
+namespace balleprojet
 {
+    /// <summary>
+    /// Déclaration de la classe "Player"
+    /// </summary>
     internal class Player
     {
-        public string Name { get; set; }
-        public int Lives { get; set; }
-        public int Score { get; set; }
+        // Propriétés publiques de la classe Player
 
+        /// <summary>
+        /// Nom du joueur
+        /// </summary>
+        public string Name { get; set; }    
+
+        /// <summary>
+        /// Nombre de vies du joueur
+        /// </summary>
+        public int Lives { get; set; }      
+
+        /// <summary>
+        /// Score du joueur
+        /// </summary>
+        public int Score { get; set; } 
+
+        /// <summary>
+        /// Constructeur de la classe Player
+        /// </summary>
+        /// <param name="name">Nom</param>
         public Player(string name)
         {
-            Name = name;
-            Lives = 6;
-            Score = 0;
+            Name = name;    // Initialise la propriété name avec le paramètre name
+            Lives = 6;      // Initialise la propriété Lives à 6
+            Score = 0;      // Initialise la propriété Score à 0
         }
     }
 }
