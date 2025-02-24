2f2320226c1c6797586d935388bd60762c409617
Jazztel20 <pb58unc@eduvaud.ch>
2025-02-24 14:43:46 +0100

    chore: ajout de commentaires pour presque toutes les lignes de code de la classe

--- balleprojet/Program.cs
+++ balleprojet/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace balleprojet
+﻿/********************************************************************************
+ Programmation Orientée Objet en C#
+ Projet: Jeu de la balle
+ Prénom et nom : Matias Denis
+ Cours: I320
+ Classe: FID1
+ *******************************************************************************/
+
+// Déclaration de l'espace de noms "balleprojet"
+namespace balleprojet
 {
+    /// <summary>
+    /// Déclaration de la classe "Program"
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Déclaration de la méthode principale "Main"
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Création d'une instance de la classe "Game"
             Game game = new Game();
+
+            // Appel de la méthode "Start" de l'objet "game" pour démarrer le jeu
             game.Start();
+
+            // Encapsulation à revoir !!!!
         }
     }
 }
