749e6f1c14759132e45fe26aa18bd254522b3987
Jazztel20 <pb58unc@eduvaud.ch>
2025-02-24 14:42:30 +0100

    feat: ajout de la condition if (nextY > posY)pour changer la balle de couleur lorsqu'elle redescend

--- balleprojet/Ball.cs
+++ balleprojet/Ball.cs
@@ -1,65 +1,124 @@
-﻿using System;
+﻿/********************************************************************************
+ Programmation Orientée Objet en C#
+ Projet: Jeu de la balle
+ Prénom et nom : Matias Denis
+ Cours: I320
+ Classe: FID1
+ *******************************************************************************/
 
+using System;
+
+// Déclaration de l'espace de noms "balleprojet"
 namespace balleprojet
 {
+    /// <summary>
+    /// Déclaration de classe "Ball"
+    /// </summary>
     internal class Ball
     {
+        // Déclarations publiques de la classe "Ball"
+
+        /// <summary>
+        /// Angle de tir de la balle
+        /// </summary>
         public int Angle { get; set; }
+
+        /// <summary>
+        /// Puissance de tir de la balle
+        /// </summary>
         public int Power { get; set; }
 
-        public Ball(int angle, int power)
+        /// <summary>
+        /// ??
+        /// </summary>
+        public ConsoleColor Color { get => _color; set => _color = value; }
+
+        /// <summary>
+        /// Constructeur de la classe "Ball"
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <param name="power"></param>
+        
+        // ??
+        private ConsoleColor _color = ConsoleColor.Green;
+
+        /// <summary>
+        /// Constructeur de la classe "Ball"
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <param name="power"></param>
+        /// <param name="color"></param>
+        public Ball(int angle, int power, ConsoleColor color)
         {
-            Angle = angle;
-            Power = power;
+            Angle = angle;      // Initialise la propriété Angle avec le paramètre angle
+            Power = power;      // Initialise la propriété Power avec le paramètre power
+            _color = color;     // Initialise la couleur 
         }
 
+        /// <summary>
+        /// Méthode pour calculer la trajectoire de la balle
+        /// </summary>
+        /// <param name="startX"></param>
+        /// <param name="startY"></param>
+        /// <param name="power"></param>
+        /// <param name="isRight"></param>
+        /// <param name="player1"></param>
+        /// <param name="player2"></param>
+        /// <param name="wall1"></param>
+        /// <param name="wall2"></param>
         public void CalculateTrajectory(int startX, int startY, int power, bool isRight, Player player1, Player player2, Wall wall1, Wall wall2)
         {
             // Constantes de la simulation
-            double gravity = 9.81; // m/s^2
-            double angleInRadians = Angle * (Math.PI / 180); // Conversion en radians
-            double initialVelocity = power * 0.5; // Vitesse initiale proportionnelle à la puissance
-            double timeStep = 0.1; // Intervalle de temps pour la simulation
-            double t = 0.0; // Temps initial
+            double gravity = 9.81;                              // m/s^2
+            double angleInRadians = Angle * (Math.PI / 180);    // Conversion en radians
+            double initialVelocity = power * 0.5;               // Vitesse initiale proportionnelle à la puissance
+            double timeStep = 0.1;                              // Intervalle de temps pour la simulation
+            double t = 0.0;                                     // Temps initial
+            double posX = startX;                               // Position initiale en X
+            double posY = startY;                               // Position initiale en Y
 
-            double posX = startX; // Position initiale en X
-            double posY = startY; // Position initiale en Y
-
-            while (posY <= 32 && posX >= 0 && posX <= 150) // Tant que la balle est dans les limites du terrain
+            // Boucle qui fonctionne que tant que la balle est dans les limites du terrain
+            while (posY <= 32 && posX >= 0 && posX <= 150) 
             {
-                // Calcul des positions
+                // Calcul des positions suivantes de la balle
                 double nextX = isRight ? (startX + initialVelocity * Math.Cos(angleInRadians) * t) : (startX - initialVelocity * Math.Cos(angleInRadians) * t);
                 double nextY = startY - (initialVelocity * Math.Sin(angleInRadians) * t - 0.5 * gravity * t * t);
+                if (nextY > posY)
+                {
+                    Color = ConsoleColor.Red;
+                }
 
                 // Affichage de la position de la balle
                 Console.SetCursorPosition((int)nextX, (int)nextY);
+                Console.ForegroundColor = _color;
                 Console.Write('o');
+                Console.ResetColor();
 
                 // Vérification des collisions avec le mur adverse
-                if (isRight && (int)nextX >= 110 && (int)nextX <= 113 && (int)nextY >= 25 && (int)nextY <= 30) // Joueur 1 tire sur le mur du joueur 2
+                if (isRight && (int)nextX >= 110 && (int)nextX <= 113 && (int)nextY >= 25 && (int)nextY <= 30)      // Joueur 1 tire sur le mur du joueur 2
                 {
-                    wall2.Hit((int)nextY - 25); // Disparaître la case touchée
-                    player1.Score++; // Le joueur 1 marque un point
+                    wall2.Hit((int)nextY - 25);                                                                     // Disparaître la case touchée
+                    player1.Score++;                                                                                // Le joueur 1 marque un point
                     break;
                 }
-                else if (!isRight && (int)nextX >= 35 && (int)nextX <= 38 && (int)nextY >= 25 && (int)nextY <= 30) // Joueur 2 tire sur le mur du joueur 1
+                else if (!isRight && (int)nextX >= 35 && (int)nextX <= 38 && (int)nextY >= 25 && (int)nextY <= 30)  // Joueur 2 tire sur le mur du joueur 1
                 {
-                    wall1.Hit((int)nextY - 25); // Disparaître la case touchée
-                    player2.Score++; // Le joueur 2 marque un point
+                    wall1.Hit((int)nextY - 25);     // Disparaître la case touchée
+                    player2.Score++;                // Le joueur 2 marque un point
                     break;
                 }
 
                 // Vérification des collisions avec le joueur adverse
                 if (isRight && (int)nextX >= 125 && (int)nextX <= 130 && (int)nextY >= 32 && (int)nextY <= 34) // Joueur 1 tire sur le joueur 2
                 {
-                    player2.Lives--; // Le joueur 2 perd une vie
-                    player1.Score++; // Le joueur 1 marque un point
+                    player2.Lives--;        // Le joueur 2 perd une vie
+                    player1.Score++;        // Le joueur 1 marque un point
                     break;
                 }
                 else if (!isRight && (int)nextX >= 20 && (int)nextX <= 25 && (int)nextY >= 32 && (int)nextY <= 34) // Joueur 2 tire sur le joueur 1
                 {
-                    player1.Lives--; // Le joueur 1 perd une vie
-                    player2.Score++; // Le joueur 2 marque un point
+                    player1.Lives--;        // Le joueur 1 perd une vie
+                    player2.Score++;        // Le joueur 2 marque un point
                     break;
                 }
 
